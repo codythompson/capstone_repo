7bebe2b2be31e12871a3ae7fc0a6ec391fcbbe0b
Cody Thompson <cdt27@nau.edu>
2012-12-08 12:39:15 -0700

    changed the install script to remove the existing CTX Tools directory before copying the CTX tools found in the repo

--- galaxy/install_to_galaxy.sh
+++ galaxy/install_to_galaxy.sh
@@ -6,5 +6,6 @@ else
     cp -f tool_conf.xml $1/tool_conf.xml
     cp -f datatypes_conf.xml $1/datatypes_conf.xml
     cp -f images.py $1/lib/galaxy/datatypes/images.py
+    rm -rf $1/tools/CTXTestTools
     cp -rf tools/CTXTestTools $1/tools/CTXTestTools
 fi
