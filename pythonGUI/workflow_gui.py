92272593b631fef5b7312431d4ce18f55626e308
Cody <cody.thompson22@gmail.com>
2012-12-12 17:22:31 -0700

    The GUI - while not actually hooked up to anything has a working add tool and delete tool function now

--- pythonGUI/workflow_gui.py
+++ pythonGUI/workflow_gui.py
@@ -1,26 +1,57 @@
 from Tkinter import *
 
 def load_available_tools():
-    return ["tool A", "tool B", "tool C", "tool D", "tool F"]
+    return [("tool A", "obj"), ("tool B", "obj"), ("tool C", "obj"),
+            ("tool D", "obj"), ("tool F", "obj")]
 
 class ToolBox:
-    def __init__(self, root, tools=None):
+    def __init__(self, root, tool_tuples = None):
         self.listbox = Listbox(root)
-        if tools:
-            self.insert_tools(tools)
+        self.tool_tuples = []
+        if tool_tuples:
+            self.insert_tools(tool_tuples)
 
-    def insert_tools(self, tools):
-        for tool in tools:
-            self.listbox.insert(END, tool)
+    def insert_tools(self, tool_tuples):
+        self.tool_tuples.extend(tool_tuples)
+        for tool in tool_tuples:
+            self.listbox.insert(END, tool[0])
 
-    def curselection(self):
-        return self.listbox.curselection()
+    def insert_tool(self, tool_tuple):
+        self.tool_tuples.append(tool_tuple)
+        self.listbox.insert(END, tool_tuple[0])
+
+    def selected_index(self):
+        selected_index = self.listbox.curselection()
+        if len(selected_index) > 0:
+            return int(selected_index[0])
+        else:
+            return -1
+
+    def selected(self):
+        index = self.selected_index()
+        if index >= 0:
+            return self.tool_tuples[index]
+        else:
+            return None
+
+    def remove_selected(self):
+        index = self.selected_index()
+        if index >= 0:
+            self.listbox.delete(index)
+            self.tool_tuples.pop(index)
 
     def pack(self, **options):
         self.listbox.pack(options)
 
-def print_selection(curse_whatevs):
-    print curse_whatevs
+    def __str__(self):
+        string = "---- ToolBox instance ----\n"
+        for tool in self.tool_tuples:
+            string = string + repr(tool) + "\n"
+        string = string + "--------------------------"
+        return string
+
+def add_tool(target_tool_box, tool_tuple):
+    target_tool_box.insert_tool(tool_tuple)
 
 root = Tk()
 
@@ -29,11 +60,12 @@ toolboxL.pack(side=LEFT)
 
 buttonframe = Frame(root)
 
-moveR = Button(buttonframe, text=">>",
-        command= lambda: print_selection(toolboxL.curselection()))
-moveR.pack()
-moveL = Button(buttonframe, text="<<")
-moveL.pack()
+moveR = Button(buttonframe, text="add >>",
+        command= lambda: add_tool(toolboxR, toolboxL.selected()))
+moveR.pack(fill=X)
+moveL = Button(buttonframe, text="<< delete",
+        command = lambda: toolboxR.remove_selected())
+moveL.pack(fill=X)
 
 buttonframe.pack(side=LEFT)
 
