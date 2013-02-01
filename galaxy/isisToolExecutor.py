62e68c9b0c7c239766e2b37ca13904bd015d2f35
Cody <cody.thompson22@gmail.com>
2013-02-01 01:54:54 -0700

    The isisToolExecuter.py works on my windows machine with a simple python script that appends to the end of a text file and appends an extra file extension.

--- galaxy/isisToolExecutor.py
+++ galaxy/isisToolExecutor.py
@@ -1,6 +1,10 @@
-import sys
+import glob
+import os
+import shutil
 import subprocess
+import sys
 
+#TODO: Make this function not rely on the 'from=' label
 def get_input_filename(command_args):
     for arg in command_args:
         split_arg = arg.split("=", 1)
@@ -8,6 +12,7 @@ def get_input_filename(command_args):
             return split_arg[1]
     return None
 
+#TODO: Make this function not rely on the 'to=' label
 def get_output_filename(command_args):
     for arg in command_args:
         split_arg = arg.split("=", 1)
@@ -15,20 +20,34 @@ def get_output_filename(command_args):
             return split_arg[1]
     return None
 
+def rename_extra_extensions(file_path,
+        delete_files_with_extra_extensions = True):
+    extra_files = glob.glob(file_path + ".*")
+    if len(extra_files) > 0:
+        shutil.copyfile(extra_files[0], file_path)
+        if delete_files_with_extra_extensions:
+            for f in extra_files:
+                os.remove(f)
+
 def main():
     if len(sys.argv) < 2:
         # TODO: a better error message - maybe a usage statement
         sys.stderr.write("Error: Expected at least 2 arguments")
     else:
         command_args = sys.argv[1:]
+        input_path = get_input_filename(command_args)
+        output_path = get_output_filename(command_args)
 
         #TEST CODE - REMOVE LATER
-        print command_args
-        print "input fname: " + get_input_filename(command_args)
-        print "output fname: " + get_output_filename(command_args)
+        print "command: " + command_args[0]
+        print "input fname: " + input_path
+        print "output fname: " + output_path
 
-#        sub_proc = subprocess.Popen(command_args)
-#        sub_proc.communicate()
+        sub_proc = subprocess.Popen(command_args)
+        sub_proc.communicate()
+
+        if not os.path.exists(output_path):
+            rename_extra_extensions(output_path)
 
 if __name__ == '__main__':
     main()
--- pythonGUI/testTools/b.py
+++ pythonGUI/testTools/b.py
@@ -2,14 +2,31 @@ from sys import argv
 from sys import stdout
 import time
 
+def get_input_filename(command_args):
+    for arg in command_args:
+        split_arg = arg.split("=", 1)
+        if split_arg[0] == "from":
+            return split_arg[1]
+    return None
+
+#TODO: Make this function not rely on the 'to=' label
+def get_output_filename(command_args):
+    for arg in command_args:
+        split_arg = arg.split("=", 1)
+        if split_arg[0] == "to":
+            return split_arg[1]
+    return None
+
 if len(argv) != 3:
     print "USAGE: python a.py <input filename> <output filename>"
 else:
-    f_in = open(argv[1], "r")
+    in_name = get_input_filename(argv)
+    out_name = get_output_filename(argv)
+    f_in = open(in_name, "r")
     lines = f_in.readlines()
     lines.append("RAN: b\n")
 
-    f_out = open(argv[2], "w")
+    f_out = open(out_name + ".mmk", "w")
     f_out.writelines(lines)
 
     print "TOOL B"
