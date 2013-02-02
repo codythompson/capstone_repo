b65c213eb8e7819a186b3f751c4d744ac0a23f9c
isis3 user <i3user@localhost.localdomain>
2013-02-02 14:25:23 -0700

    Modified install script to install universe_wsgi.ini and add Isis directory of images.

--- galaxy/install_to_galaxy.sh
+++ galaxy/install_to_galaxy.sh
@@ -9,8 +9,15 @@ else
     echo "Copying datatypes_conf.xml to $1/datatypes_conf.xml" 
     cp -f datatypes_conf.xml $1/datatypes_conf.xml
 
+    echo "Copying universe_wsgi.ini to $1/universe_wsgi.ini" 
+    cp -f universe_wsgi.ini $1/universe_wsgi.ini
+
     echo "Copying images.py to $1/lib/galaxy/datatypes/images.py" 
     cp -f images.py $1/lib/galaxy/datatypes/images.py
+
+    echo "Copying File Directory of images to $1/database/files/Isis" 
+    rm -rf $1/database/files/Isis
+    cp -rf File_Directory/Isis $1/database/files/Isis
 
     echo "Copying the directory and contents of /tools/CTXTestTools/ to $1/tools/CTXTestTools/" 
     rm -rf $1/tools/CTXTestTools
