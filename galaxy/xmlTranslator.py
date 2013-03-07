2e68974cb56c4b3a2cb933f3dc08eb5ec37cafcb
Kolby <i3user@localhost.localdomain>
2013-03-07 16:59:55 -0700

    Updated xmlTranslator to create a new file.  Also reads line by line and reads the first word of the line for translation purposes

--- galaxy/xmlTranslator.py
+++ galaxy/xmlTranslator.py
@@ -9,17 +9,39 @@ This module will then add the file into Galaxy automatically.
 
 Created by Kolby Chien 27.2.2013
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+import sys
 
 #global variable storing command line args
 commandLine = ""
 
 #Parse ISIS XML file and generate galaxy XML file
 def parseFile(inputFile):
-	f = open(inputFile, "r")
+	f = open(inputFile, "r") #open specified input file
+
+	#Loop reads file line by line.  For each line, the first word is
+	#read to determine whether it is a name, input, parameter, 
+	#description, etc.
+	for line in f.readlines():
+		line.strip()
+		words = line.split(" ")
+		print words[0]
 	f.close()
 
+#Create a equivalent galaxy file
+def createGalaxyFile():
+	outputFile = ""
+	temp = sys.argv[1]
+	for char in temp:
+		if char is ".":
+			break
+		else:
+			outputFile += char
+	galaxyFile = open(outputFile, "w")
+			
+
+'''
 #Convert tool name
-def toolName():
+def toolName()
 
 #Convert Description
 def toolDescribe():
@@ -36,4 +58,9 @@ def convertParams():
 #Add tool to galaxy.
 #Done after parsing
 def toolToGalaxy():
+'''
+
+#createGalaxyFile()
+parseFile(sys.argv[1])
+
 	
