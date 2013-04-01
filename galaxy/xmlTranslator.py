55f60f0d20cfe3fb772fa58757926cffa77ae0a8
Kolby <i3user@localhost.localdomain>
2013-03-31 20:28:42 -0700

    Xml translator partially modified to translate isis to galaxy xml files.  Currently need to convert params and outputs.

--- galaxy/xmlTranslator.py
+++ galaxy/xmlTranslator.py
@@ -12,9 +12,6 @@ Created by Kolby Chien 27.2.2013
 import sys
 import xml.etree.ElementTree as ET
 
-#global variable storing command line args
-commandLine = ""
-
 #Parse ISIS XML file and generate galaxy XML file
 def parseFile(inputFile):
 	tree = ET.parse(inputFile)
@@ -22,37 +19,67 @@ def parseFile(inputFile):
 	name = root.get("name")
 	toolFileName = name
 
+	#Determine tool name and create outer XML element
    	toolName(name, toolFileName)
 
+	#Tool description
 	description = root.find("brief").text
 	toolDescribe(description, toolFileName)
 
 	outputPresent = 1 #If there is an output, is 0, otherwise 1
 
-	#Parses through parameters to find if there is an output file.  
+	#Parses through xml file to find inputs and output 
+	inputs = []
+	params = []
 	for child in root.find("groups").find("group"):
+		#If an output file exists, set the flag
 		if child.get("name") == "TO":
 			outputPresent = 0
-	comLine(name, toolFileName, outputPresent) #TODO add parameters
+		inputName = child.get("name")
+		inputs.append(inputName)
+	for child in root.find("groups"):
+		if child.get("name") != "Files":
+			for child in child:
+				paramName = child.get("name")
+				params.append(paramName)
+	comLine(name, toolFileName, inputs, params, outputPresent) 
 
-	inputType = ""
+	#Convert isis inputs to galaxy format
+	inputFileType = []
 	for child in root.find("groups").find("group"):
-		if child.get("name") == "FROM":
+		try:
 			inputType = child.find("filter").text
 			inputType = inputType.strip()
-	inputType = inputType[2:]
+			inputType = inputType[2:]
+			inputFileType.append(inputType)
+		except AttributeError:
+			inputType = child.find("type").text
+			if inputType == "cube":
+				inputFileType.append("cub")
+	convertInput(inputs, inputFileType, toolFileName)
 
-	convertInput(inputType, toolFileName)
-
+	#Convert tool parameters to Galaxy
+	paramType = []
+	paramMin = []
+	paramDefault = []
+	for child in root.find("groups"):
+		if child.get("name") != "Files":
+			for child in child:
+				paramName = child.get("name")
+				params.append(paramName)
 	convertParams(toolFileName)
 
+	#Determine tool's output file type
 	outputType = ""
 	if outputPresent is 0:
 		for child in root.find("groups").find("group"):
 			if child.get("name") == "TO":
-				outputType = child.find("filter").text
-				outputType = outputType.strip()
-		outputType = outputType[2:]
+				try:
+					outputType = child.find("filter").text
+					outputType = outputType.strip()
+					outputType = outputType[2:]
+				except AttributeError:
+					outputType = "cub"
 	else:
 		outputType = "cub"
 
@@ -66,7 +93,7 @@ def createGalaxyFile():
 	if len(sys.argv) < 1:
 		print "Usage: Accepts 1 xml file"
 		exit()
-	elif len(sys.argv) > 1:
+	elif len(sys.argv) > 2:
 		print "Usage: Accepts 1 xml file"
 		exit()
 	else:
@@ -86,7 +113,6 @@ def toolName(name, toolFile):
 
 
 #Convert Description
-#TODO format text to remove intermediate tabs
 def toolDescribe(text, toolFile):
 	text = text.strip()
 	toolDesc = '\n\t<description>' + text  + '</description>'
@@ -97,27 +123,48 @@ def toolDescribe(text, toolFile):
 
 #Convert Command Line
 #TODO add parameter functions and fix if statement to check for output
-def comLine(name, toolFile, outputPresent):
+def comLine(name, toolFile, inputs, params, outputPresent):
+	#compile inputs and output files for command line
+	inputString = ""
+	inputs = list(reversed(inputs))
+	while len(inputs) > 0:
+		temp = inputs.pop()
+		inputString += temp + "=$" + temp + ' '
+
+	#compile params for command line
+	paramString = ""
+	params = list(reversed(params))
+	while len(params) > 0:
+		temp = params.pop()
+		paramString += temp + "=$" + temp + ' '
 	if outputPresent is 1:
-		comLine = '\n\t<command interpreter="python">isisToolExecutor.py in_is_out=true to=$to start ' + name + ' from=$input</command>'
+		comLine = '\n\t<command interpreter="python">isisToolExecutor.py in_is_out=true to=$to start ' + name + ' ' + inputString + paramString + '</command>'
 	else:
-		comLine = '\n\t<command interpreter="python">isisToolExecutor.py ' + name + ' from=$input' + ' to=$output</command>'
+		comLine = '\n\t<command interpreter="python">isisToolExecutor.py ' + name + ' ' + inputString + paramString + '</command>'
 	galaxyFile = open(toolFile, "a")
 	galaxyFile.write(comLine)
 	galaxyFile.close()
 
 
 #Convert Input
-#TODO test
-def convertInput(inputType, toolFile):
-	inputs = '\n\t<inputs>\n' + '\t\t<param format="' + inputType + '" name="input" type="data" label="from="/>'
+def convertInput(inputName, inputType, toolFile):
 	galaxyFile = open(toolFile, "a")
+	inputs = '\n\t<inputs>'
 	galaxyFile.write(inputs)
+	for index in inputName:
+		if index == "TO":
+			pass
+		else:
+			for fileType in inputType:
+				inputs = '\n\t\t<param name="' + index + '" format="' + fileType + '" type="data" label="from="/>'
+				galaxyFile.write(inputs)
+				break
 	galaxyFile.close()
 
 
 #Convert Params
 def convertParams(toolFile):
+	
 	closeInput = '\n\t</inputs>'
 	galaxyFile = open(toolFile, "a")
 	galaxyFile.write(closeInput)
@@ -131,8 +178,7 @@ def convertOutput(outputType, toolFile):
 	galaxyFile.write(outputs)
 	galaxyFile.close()
 
-#Add help section
-#TODO Text should refer to relavent isis website
+#Adds help section to galaxy xml which refers user to relavent isis tool page
 def convertHelp(tool,toolFile):
 	help = '\n\t<help>isis.astrogeology.usgs.gov/Application/presentation/Tabbed/' + tool + '/' + tool + '.html</help>' + '\n</tool>'
 	galaxyFile = open(toolFile, "a")
