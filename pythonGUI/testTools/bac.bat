ac28275bc4010080b45543233d44e161a35d532a
Cody Thompson <cdt27@nau.edu>
2013-05-05 13:59:26 -0700

    Removed pythonGUI as it is not part of the final solution

--- pythonGUI/testTools/bac.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-python b.py %1 %1_out.txt
-python a.py %1_out.txt
-python c.py %1_out.txt %1_out_c.txt
--- pythonGUI/testTools/d.py
+++
@@ -1,7 +0,0 @@
-from sys import argv
-
-if len(argv) != 2:
-    print "USAGE: python d.py <input filename>"
-else:
-    f = open(argv[1], "a")
-    f.write("RAN: d\n")
